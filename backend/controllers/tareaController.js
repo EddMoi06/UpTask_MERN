fc0e29385b159763e691e548dd15b47fee96461d
EddMoi06 <145163602+EddMoi06@users.noreply.github.com>
2023-12-21 16:13:18 -0400

    modified:   backend/controllers/tareaController.js

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -7,14 +7,37 @@ const agregarTarea = async (req,res) => {
 
     if(!existeProyecto){
         const error = new Error('El Proyecto no Existe')
-        res.status(404).json({msg: error.message})
+        return res.status(404).json({msg: error.message})
     }
 
-    
+    if(existeProyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error('No tiene los permisos')
+        return res.status(403).json({msg: error.message})
+    }
+
+    try {
+        const tareaAlmacenada = await Tarea.create(req.body)
+        res.json(tareaAlmacenada)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const obtenerTarea = async(req,res) => {
+    const { id } = req.params;
+    const tarea = await Tarea.findById(id).populate('proyecto');
 
+    if(!tarea){
+        const error = new Error('Tarea no Encontrada')
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error('No tiene los permisos')
+        return res.status(403).json({msg: error.message})
+    }
+
+    res.json(tarea)
 }
 
 const actualizarTarea = async (req,res) => {
