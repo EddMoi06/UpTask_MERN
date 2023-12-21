56ffd7a6b00a8eb5b1f579e127cf1cb7b49f0ed3
EddMoi06 <145163602+EddMoi06@users.noreply.github.com>
2023-12-21 16:55:19 -0400

    modified:   backend/controllers/proyectoController.js 	modified:   backend/controllers/tareaController.js

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,4 +1,5 @@
 import Proyecto from "../models/Proyecto.js"
+import Tarea from "../models/Tarea.js"
 
 const obtenerProyectos = async (req, res) => {
     const proyectos = await Proyecto.find().where('creador').equals(req.usuario)
@@ -93,7 +94,16 @@ const eliminarColaborador = async (req, res) => {
 }
 
 const obtenerTareas = async (req, res) => {
+    const { id } = req.params;
+    const existeProyecto = await Proyecto.findById(id);
 
+    if(!existeProyecto){
+        const error = new Error('No Encontrado')
+        return res.status(404).json({msg: error.message})
+    }
+
+    const tareas = await Tarea.find().where('proyecto').equals(id);
+    res.json(tareas)
 }
 
 export {
